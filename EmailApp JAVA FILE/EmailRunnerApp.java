d22d95e2a320b163d18f4be9f3efb4f36319eb36
Shubham Bairwa <shubhamver61002@gmail.com>
2018-09-01 21:22:12 +0530

    Add files via upload

---
+++ EmailApp JAVA FILE/EmailRunnerApp.java
@@ -0,0 +1,10 @@
+package EmailApp;
+
+public class EmailRunnerApp {
+
+	public static void main(String[] args) {
+		Email em1=new Email("Shubham","Bairwa");
+		
+System.out.println("YOUR DETAILS ARE:- "+em1.showInfo());	}
+
+}
